164f6228b930b001f0cdc53eb3e37b1fdc41ae10
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-23 13:30:51 +0100

    Added get all persons

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-node_modules
+const express = require('express')
+
+const app = express()
+const PORT = 3001
+
+const persons = [
+    {
+        name: 'Arto Hellas',
+        tfno: '040-123456',
+        id: 1,
+    },
+    {
+        name: 'Ada Lovelace',
+        tfno: '39-44-5323523',
+        id: 2,
+    },
+    {
+        name: 'Dan Abramov',
+        tfno: '12-43-234345',
+        id: 3,
+    },
+    {
+        name: 'Mary Poppendieck',
+        tfno: '39-23-6423122',
+        id: 4,
+    },
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
