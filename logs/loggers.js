e047487bfccb36d76cf966b64b4b9a3d02f8a7a5
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-23 17:26:43 +0100

    Prepared to deply

--- logs/loggers.js
+++ logs/loggers.js
@@ -1,4 +1,8 @@
 const morgan = require('morgan')
+var fs = require('fs')
+var path = require('path')
+
+
 
 morgan.token('req-body', (request) => {
     return JSON.stringify(request.body)
@@ -6,9 +10,12 @@ morgan.token('req-body', (request) => {
 
 const morganFormat = ':method :url :status :res[content-length] - :response-time ms :req-body'
 
+const postLogStream = fs.createWriteStream(path.join(__dirname, 'post.log'), { flags: 'a' })
+
 const devLogger = morgan('dev')
 
 const postLogger = morgan(morganFormat, {
+    stream: postLogStream,
     skip: (req) => req.method !== 'POST' 
 })
 
